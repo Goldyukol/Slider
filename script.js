ae1cb532bcefb01fe76091d408a1143b7d0851ba
Yury <goldyukol12@gmail.com>
2019-10-25 15:19:31 +0300

    start: slider

---
+++ script.js
@@ -0,0 +1,39 @@
+let leftImg = document.getElementById('left-img');
+let rightImg = document.getElementById('right-img');
+let slideImg = document.getElementById('slide-img')
+
+leftImg.addEventListener('click', onShowLeftImg);
+rightImg.addEventListener('click', onShowRightImg);
+
+let imageArray = [];
+imageArray.push('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQx6kbyGsin9D0M0GstdLyeYnRxtMkTHx8fCHi1Rf_tPDhDvcmT');
+imageArray.push('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJdkgTBQc2qWOkzlvG6_GIMZOyiBKOcjTlw77zE-VEDQJSguvE');
+imageArray.push('https://i.ytimg.com/vi/YizPeVoFDCs/hqdefault.jpg');
+imageArray.push('https://crdms.images.consumerreports.org/c_scale,f_auto,h_250,w_355/prod/cars/cr/car-groups/107-11362');
+
+let currentIndex = 0;
+
+slideImg.src = imageArray[currentIndex];
+
+
+function onShowLeftImg() {
+    currentIndex--;
+    slideImg.src = imageArray[currentIndex];
+    rightImg.disabled = false
+
+    if (currentIndex === 0) {
+        leftImg.disabled = true;
+
+    }
+}
+
+function onShowRightImg() {
+    currentIndex++;
+    slideImg.src = imageArray[currentIndex];
+    leftImg.disabled = false;
+
+    if (currentIndex === imageArray.length - 1) {
+        rightImg.disabled = true;
+
+    }
+}
